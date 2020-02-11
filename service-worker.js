0178029656ec0b04548501e3cb0960feaf888ea1
Gabriel Oliveira Silva <48599711+GabrielOS2512@users.noreply.github.com>
2020-02-11 17:32:10 -0300

    Commit1
    Primeiro Commit para teste de funcionamento

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+// SW Version
+const version = '1.0';
+
+// Static Cache - App Shell
+const appAssets = [
+    '/index.html',
+    '/tela2.html',
+    '/tela3.html',
+    '/css/bootstrap.css',
+    '/js/bootstrap.js',
+];
+
+// SW Install
+self.addEventListener( 'install', e => {
+    e.waitUntil(
+        caches.open( `static-${version}` )
+            .then( cache => cache.addAll(appAssets) )
+    );
+});
+
+// SW Activate
+self.addEventListener( 'activate', e => {
+
+    // Clean static cache
+    let cleaned = caches.keys().then( keys => {
+        keys.forEach( key => {
+            if ( key !== `static-${version}` && key.match('static-') ) {
+                return caches.delete(key);
+            }
+        });
+    });
+    e.waitUntil(cleaned);
+});
+
+// Network with Cache Fallback
+const fallbackCache = (req) => {
+
+    // Try Network
+    return fetch(req).then( networkRes => {
+
+        // Check res is OK, else go to cache
+        if( !networkRes.ok ) throw 'Fetch Error';
+
+        // Update cache
+        caches.open( `static-${version}` )
+            .then( cache => cache.put( req, networkRes ) );
+
+        // Return Clone of Network Response
+        return networkRes.clone();
+    })
+
+    // Try cache
+    .catch( err => caches.match(req) );
+};
+
+self.addEventListener('fetch', e => {
+
+    // App shell
+    if( e.request.url.match(location.origin) ) {
+        e.respondWith( fallbackCache(e.request) );
+      }
+});
+
+console.log("Rodou");
